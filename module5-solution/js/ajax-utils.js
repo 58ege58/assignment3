e6ccb58d1a8aab39175b0afbbd9579796754707c
mehmet.ege.demir1 <mhmtege29@gmail.com>
2026-03-22 03:09:24 +0300

    ajax-utils.js

--- module5-solution/js/ajax-utils.js
+++ module5-solution/js/ajax-utils.js
@@ -1,26 +1,16 @@
 (function (global) {
-
-// Set up a namespace for our utility
 var ajaxUtils = {};
 
-
-// Returns an HTTP request object
 function getRequestObject() {
-  if (window.XMLHttpRequest) {
+  if (global.XMLHttpRequest) {
     return (new XMLHttpRequest());
-  } 
-  else if (window.ActiveXObject) {
-    // For very old IE browsers (optional)
-    return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
   else {
     global.alert("Ajax is not supported!");
-    return(null); 
+    return (null); 
   }
 }
 
-
-// Makes an Ajax GET request to 'requestUrl'
 ajaxUtils.sendGetRequest = 
   function(requestUrl, responseHandler, isJsonResponse) {
     var request = getRequestObject();
@@ -31,20 +21,15 @@ ajaxUtils.sendGetRequest =
                        isJsonResponse); 
       };
     request.open("GET", requestUrl, true);
-    request.send(null); // for POST only
+    request.send(null);
   };
 
-
-// Only calls user provided 'responseHandler'
-// function if response is ready
-// and not an error
 function handleResponse(request,
                         responseHandler,
                         isJsonResponse) {
   if ((request.readyState == 4) &&
      (request.status == 200)) {
 
-    // Default to isJsonResponse = true
     if (isJsonResponse == undefined) {
       isJsonResponse = true;
     }
@@ -58,9 +43,6 @@ function handleResponse(request,
   }
 }
 
-
-// Expose utility to the global object
 global.$ajaxUtils = ajaxUtils;
-
 
 })(window);
